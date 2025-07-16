bd4cf13fcf937729a3f24367b473538ad06728f5
Ashuma Saalam <ShabistaSaalam@users.noreply.github.com>
2025-07-16 17:34:47 +0530

    Updated API URL for ADMIN page

--- src/api/applications.ts
+++ src/api/applications.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
-
+const API_ADMIN_URL= import.meta.env.VITE_API_ADMIN_URL || "http://localhost:5001";
 export interface ApplicationData {
   job_id: string;
   name: string;
@@ -16,6 +16,6 @@ export async function submitApplication(data: ApplicationData) {
 }
 
 export async function fetchApplications() {
-  const response = await axios.get(`${API_BASE_URL}/applications`);
+  const response = await axios.get(`${API_ADMIN_URL}`);
   return response.data;
 }
